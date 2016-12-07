133ba4a56850fb2e23c218d43d8593cb95c4de2b
徐静 <bjxujing1@corp.netease.com>
2016-12-07 20:07:40 +0800

    使用react-scrollbar

--- src/components/slide1.js
+++ src/components/slide1.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './slide-header'
 import Footer from './slide-footer'
+import ScrollArea from 'react-scrollbar';
 
 export default React.createClass({
 	getInitialState () {
@@ -12,17 +13,48 @@ export default React.createClass({
 	},
 	render () {
 		return (
-			<div className="m-slide">
-				<Header className="title">Learn React and Write Some Demos</Header>
-				
-				<div className="m-content"></div>
+			<div>
+				<div className="m-slide">
+					<Header className="title">Learn React and Write Some Demos</Header>
+					
+					<div className="m-content"></div>
 
-				<div className="m-sign">
-					<p>asyxu</p>
-					<p>2016.10.28</p>
+					<div className="m-sign">
+						<p>asyxu</p>
+						<p>2016.10.28</p>
+					</div>
+
+					<Footer first={this.state.first} last={this.state.last} index={this.state.index} /> 
 				</div>
 
-				<Footer first={this.state.first} last={this.state.last} index={this.state.index} /> 
+				<ScrollArea
+					speed={0.8}
+					className="area"
+					contentClassName="content"
+					horizontal={true}
+					style={{height:'50px',paddinBottom:'40px', display:'none'}}
+				>
+					<div>Some long content.<br/>
+					Some long content.<br/>
+					Some long content.<br/>
+					Some long content.<br/>
+					Some long content.<br/>
+					Some long content.<br/>
+					Some long content.<br/>
+					Some long content.<br/>
+					Some long content.<br/>
+					Some long content.<br/>
+					Some long content.<br/>
+					Some long content.<br/>
+					Some long content.<br/>
+					Some long content.<br/>
+					Some long content.<br/>
+					Some long content.<br/>
+					Some long content.<br/>
+					Some long content.<br/>
+					Some long content.<br/>
+					</div>
+				</ScrollArea>
 			</div>
 		)
 	}
